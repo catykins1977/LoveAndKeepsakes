969a4f8e993c84deeab8f035a5b7cb24a7878d46
catykins1977 <catykins@googlemail.com>
2015-11-15 22:07:35 +0000

    Addition of lightbox functionality

--- LoveAndKeepsakes/Love-and-Keepsakes/Scripts/LandK.js
+++ LoveAndKeepsakes/Love-and-Keepsakes/Scripts/LandK.js
@@ -1,93 +1,109 @@
-﻿$(document).ready(function () {
-    $('#contact_form').bootstrapValidator({
-        // To use feedback icons, ensure that you use Bootstrap v3.1.0 or later
-        feedbackIcons: {
-            valid: 'glyphicon glyphicon-ok',
-            invalid: 'glyphicon glyphicon-remove',
-            validating: 'glyphicon glyphicon-refresh'
-        },
-        fields: {
-            full_name: {
-                validators: {
-                    stringLength: {
-                        min: 2,
-                    },
-                    notEmpty: {
-                        message: 'Please supply your full name'
-                    }
-                }
-            },
-            
-            email: {
-                validators: {
-                    notEmpty: {
-                        message: 'Please supply your email address'
-                    },
-                    emailAddress: {
-                        message: 'Please supply a valid email address'
-                    }
-                }
-            },
-            phone: {
-                validators: {
-                    notEmpty: {
-                        message: 'Please supply your phone number'
-                    },
-                    phone: {
-                        country: 'UK',
-                        message: 'Please supply a vaild phone number with area code'
-                    }
-                }
-            },
-            country: {
-                validators: {
-                    notEmpty: {
-                        message: 'Please select your state'
-                    }
-                }
-            },
-            postal: {
-                validators: {
-                    notEmpty: {
-                        message: 'Please supply your zip code'
-                    },
-                    postalCode: {
-                        country: 'UK',
-                        message: 'Please supply a vaild zip code'
-                    }
-                }
-            },
-            message: {
-                validators: {
-                    stringLength: {
-                        min: 10,
-                        max: 200,
-                        message: 'Please enter at least 10 characters and no more than 200'
-                    },
-                    notEmpty: {
-                        message: 'Please supply a description of your project'
-                    }
-                }
-            }
-        }
-    })
-        .on('success.form.bv', function (e) {
-            $('#success_message').slideDown({ opacity: "show" }, "slow") // Do something ...
-            $('#contact_form').data('bootstrapValidator').resetForm();
-
-            // Prevent form submission
-            e.preventDefault();
-
-            // Get the form instance
-            var $form = $(e.target);
-
-            // Get the BootstrapValidator instance
-            var bv = $form.data('bootstrapValidator');
-
-            // Use Ajax to submit form data
-            $.post($form.attr('action'), $form.serialize(), function (result) {
-                console.log(result);
-            }, 'json');
+﻿$(document).ready(function(){
+    $('li img').on('click', function(){
+        var src = $(this).attr('src');
+        var img = '<img src"'+ src + '"class=img-responsive"/>';
+        $('#galleryModal').modal();
+        $('#galleryModal').on('shown.bs.modal', function(){
+            $('#galleryModal.modal-body').html(img);
         });
-});
+        $('#galleryModal').on('hidden.bs.modal', function(){
+            $('#galleryModal .modal-body').html('');
+        });
+    });
+})
+
+
+
+//$(document).ready(function () {
+//    $('#contact_form').bootstrapValidator({
+//        // To use feedback icons, ensure that you use Bootstrap v3.1.0 or later
+//        feedbackIcons: {
+//            valid: 'glyphicon glyphicon-ok',
+//            invalid: 'glyphicon glyphicon-remove',
+//            validating: 'glyphicon glyphicon-refresh'
+//        },
+//        fields: {
+//            full_name: {
+//                validators: {
+//                    stringLength: {
+//                        min: 2,
+//                    },
+//                    notEmpty: {
+//                        message: 'Please supply your full name'
+//                    }
+//                }
+//            },
+            
+//            email: {
+//                validators: {
+//                    notEmpty: {
+//                        message: 'Please supply your email address'
+//                    },
+//                    emailAddress: {
+//                        message: 'Please supply a valid email address'
+//                    }
+//                }
+//            },
+//            phone: {
+//                validators: {
+//                    notEmpty: {
+//                        message: 'Please supply your phone number'
+//                    },
+//                    phone: {
+//                        country: 'UK',
+//                        message: 'Please supply a vaild phone number with area code'
+//                    }
+//                }
+//            },
+//            country: {
+//                validators: {
+//                    notEmpty: {
+//                        message: 'Please select your state'
+//                    }
+//                }
+//            },
+//            postal: {
+//                validators: {
+//                    notEmpty: {
+//                        message: 'Please supply your zip code'
+//                    },
+//                    postalCode: {
+//                        country: 'UK',
+//                        message: 'Please supply a vaild zip code'
+//                    }
+//                }
+//            },
+//            message: {
+//                validators: {
+//                    stringLength: {
+//                        min: 10,
+//                        max: 200,
+//                        message: 'Please enter at least 10 characters and no more than 200'
+//                    },
+//                    notEmpty: {
+//                        message: 'Please supply a description of your project'
+//                    }
+//                }
+//            }
+//        }
+//    })
+//        .on('success.form.bv', function (e) {
+//            $('#success_message').slideDown({ opacity: "show" }, "slow") // Do something ...
+//            $('#contact_form').data('bootstrapValidator').resetForm();
+
+//            // Prevent form submission
+//            e.preventDefault();
+
+//            // Get the form instance
+//            var $form = $(e.target);
+
+//            // Get the BootstrapValidator instance
+//            var bv = $form.data('bootstrapValidator');
+
+//            // Use Ajax to submit form data
+//            $.post($form.attr('action'), $form.serialize(), function (result) {
+//                console.log(result);
+//            }, 'json');
+//        });
+//});
 
